9c21d865a6f0d0723473880f9dc98f966283cc59
Roberto Lizana <roberto@trey.es>
2020-03-30 14:30:35 +0200

    [UPD] refactorize && ctrl command && prometheus collector

---
+++ respyrator/serial.py
@@ -0,0 +1,87 @@
+##############################################################################
+# For copyright and license notices, see LICENSE file in root directory
+##############################################################################
+import os
+import pty
+import serial
+import time
+import sys
+import glob
+import threading
+from .data_frame import RxFrame, DataFrame
+
+
+def serial_ports_get():
+    if sys.platform.startswith('win'):
+        ports = ['COM%s' % (i + 1) for i in range(256)]
+    elif sys.platform.startswith('linux') or sys.platform.startswith('cygwin'):
+        ports = glob.glob('/dev/tty[A-Za-z]*')
+    elif sys.platform.startswith('darwin'):
+        ports = glob.glob('/dev/tty.*')
+    else:
+        raise EnvironmentError('Unsupported platform')
+
+    result = []
+    for port in ports:
+        try:
+            s = serial.Serial(port)
+            s.close()
+            result.append(port)
+        except (OSError, serial.SerialException):
+            pass
+    return result
+
+
+def serial_port_frame_get(port, timeout=3):
+    def test(result, port):
+        with serial.Serial(port, timeout=0.1) as s:
+            df = DataFrame(s)
+            result.append(df.read())
+
+    result = []
+    thread = threading.Thread(target=test, args=[result, port])
+    thread.daemon = True
+    thread.start()
+    time.sleep(timeout)
+    threading.currentThread().ident
+    return result and result[0] or None
+
+
+def serial_discovery_port():
+    ports = serial_ports_get()
+    for port in ports:
+        frame = serial_port_frame_get(port)
+        if frame and isinstance(frame, RxFrame):
+            return port
+    return None
+
+
+class FakeSerial:
+    def __init__(self, file_name, sleep=0.2):
+        self._waiting = True
+        master, slave = pty.openpty()
+        self.serial = serial.Serial(os.ttyname(slave))
+        self.sleep = sleep
+        if not os.path.exists(file_name):
+            raise Exception(
+                'File "%s" with samples frames not exists' % file_name)
+        self.file_name = file_name
+
+    @property
+    def in_waiting(self):
+        return self._waiting
+
+    def read(self, size):
+        generator = self.generator(size)
+        return next(generator)
+
+    def generator(self, size):
+        with open(self.file_name, 'rb') as fp:
+            while True:
+                byte = fp.read(size)
+                if not byte:
+                    break
+                yield byte
+                self._waiting = True
+                time.sleep(self.sleep)
+                self._waiting = False
---
+++ tests/test_data_frame.py
@@ -0,0 +1,36 @@
+##############################################################################
+# For copyright and license notices, see LICENSE file in root directory
+##############################################################################
+from respyrator.data_frame import RxFrame, DataFrame
+from respyrator.serial import FakeSerial
+import os
+import unittest
+
+
+class TestDataFrame(unittest.TestCase):
+
+    def setUp(self):
+        super().setUp()
+
+    def path(self, *args):
+        return os.path.join(os.path.dirname(__file__), 'samples', *args)
+
+    def test_tx_frame(self):
+        self.assertEqual(RxFrame.sizeof(), 38)
+
+    def test_fake_serial(self):
+        fname = self.path('1_frame.data')
+        with open(fname, 'rb') as fp:
+            file_content = fp.read(RxFrame.sizeof())
+        serial = FakeSerial(fname)
+        serial_frame = serial.read(RxFrame.sizeof())
+        self.assertEqual(serial_frame, file_content)
+
+    def test_dataframe(self):
+        fname = self.path('1_frame.data')
+        serial = FakeSerial(fname)
+        df = DataFrame(serial)
+        frame = df.read()
+        self.assertEqual(frame.header, 0x44)
+        self.assertIn('Header', str(frame))
+        self.assertIn('Peak pressure setting', str(frame))
